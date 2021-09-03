252a20adb2c02d1a09754e81479680df8a18fcd8
JuanFTamayo <juantamayo237532@correo.itm.edu.co>
2021-09-03 11:59:18 -0500

    complete DB

--- Vehicles.API/Data/Entities/Procedure.cs
+++ Vehicles.API/Data/Entities/Procedure.cs
@@ -20,5 +20,6 @@ namespace Vehicles.API.Data.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public decimal Price { get; set; }
 
+        public ICollection<Detail> Details { get; set; }
     }
 }
--- Vehicles.API/Data/Entities/User.cs
+++ Vehicles.API/Data/Entities/User.cs
@@ -48,6 +48,7 @@ namespace Vehicles.API.Data.Entities
         [Display(Name = "Usuario")]
         public string FullName => $"{FirstName} {LastName}";
 
-        
+        public ICollection<Vehicle> Vehicles { get; set; }
+
     }
 }
