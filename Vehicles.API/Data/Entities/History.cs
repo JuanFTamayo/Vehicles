6aa15d28d0950ba5fe39413d725f4ed0c981924d
JuanFTamayo <juantamayo237532@correo.itm.edu.co>
2021-09-13 18:13:10 -0500

    details

--- Vehicles.API/Data/Entities/History.cs
+++ Vehicles.API/Data/Entities/History.cs
@@ -15,8 +15,12 @@ namespace Vehicles.API.Data.Entities
         public Vehicle Vehicle { get; set; }        
 
         [Display(Name = "Fecha")]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
-        public DateTime Date { get; set; }        
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Fecha")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}")]
+        public DateTime DateLocal => Date.ToLocalTime();
 
         [Display(Name = "Kilometraje")]
         [DisplayFormat(DataFormatString = "{0:N0}")]
