1bf89b23f6da0453971322a38a2d95bbb807b46f
JuanFTamayo <88947668+JuanFTamayo@users.noreply.github.com>
2021-10-07 17:13:32 -0500

    api controllers

---
+++ Vehicles.API/Controllers/API/DocumentTypesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Controllers.API
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    public class DocumentTypesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public DocumentTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DocumentTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DocumentType>>> GetDocumentTypes()
+        {
+            return await _context.DocumentTypes.OrderBy(x=> x.Description).ToListAsync();
+        }
+
+        // GET: api/DocumentTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DocumentType>> GetDocumentType(int id)
+        {
+            var documentType = await _context.DocumentTypes.FindAsync(id);
+
+            if (documentType == null)
+            {
+                return NotFound();
+            }
+
+            return documentType;
+        }
+
+        // PUT: api/DocumentTypes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDocumentType(int id, DocumentType documentType)
+        {
+            if (id != documentType.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(documentType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    return BadRequest("ya existe este tipo de documento");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+
+                }
+
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+
+            }
+
+            
+        }
+
+        // POST: api/DocumentTypes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DocumentType>> PostDocumentType(DocumentType documentType)
+        {
+            _context.DocumentTypes.Add(documentType);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetDocumentType", new { id = documentType.id }, documentType);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    return BadRequest("ya existe este tipo de documento");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+
+                }
+
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+
+            }
+
+            
+        }
+
+        // DELETE: api/DocumentTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDocumentType(int id)
+        {
+            var documentType = await _context.DocumentTypes.FindAsync(id);
+            if (documentType == null)
+            {
+                return NotFound();
+            }
+
+            _context.DocumentTypes.Remove(documentType);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        
+    }
+}
